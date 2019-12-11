bdab54eba8fb261de27bfafd5377649bfdf27094
李港 <1203732857@qq.com>
2019-12-11 22:59:41 +0800

    start change 12 ne

--- 实验12：邻接矩阵无向图/main.cpp
+++ 实验12：邻接矩阵无向图/main.cpp
@@ -7,17 +7,21 @@ int main () {
 #pragma warning(disable:4996)
 	freopen ("input.txt", "r", stdin);
 	cout << "请输入图的顶点数n和边数e" << endl;
-	int n, e;  cin >> n >> e;
+	int n, e;
+	cin >> n >> e;
 	adjacencyWGraph<int> AWGraph (n, noEdge);
-	cout << "请以顶点对(顶点1,顶点2，权值)的形式输入图的每一条边" << endl;
 
+	cout << "请以 顶点1 顶点2 权值 的形式输入图的每一条边" << endl;
 	for (int i = 0; i < e; i++) {
 		int v1, v2, w;  cin >> v1 >> v2 >> w;
 		edge<int>* Edge = new edge<int> (v1, v2, w);
 		AWGraph.insertEdge (Edge);
 	}
-	if (AWGraph.connected () == true) cout << "本图是连通的。" << endl;
-	else {
+
+
+	if (AWGraph.connected () == true) {
+		cout << "本图是连通的。" << endl;
+	}else {
 		int* c = new int[n + 1];
 		cout << "连通分支数是：" << AWGraph.labelComponents (c) << endl;
 		for (int i = 1; i <= n; i++)
@@ -31,7 +35,8 @@ int main () {
 	delete[] reach;
 
 
-	int* predecessor = new int[n + 1]; cout << "请输入起始点和结束点：";  int start, dest;  cin >> start >> dest;
+	int* predecessor = new int[n + 1];
+	cout << "请输入起始点和结束点：";  int start, dest;  cin >> start >> dest;
 	cout << endl << "最短路：" << AWGraph.Dijkstra (start, dest, predecessor) << endl;
 	cout << "最短路径为：";
 	n = dest;  cout << dest << " ";
